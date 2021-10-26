8ffd420b0a0d1b191bf2daf42ad57436301e8f14
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-10-26 21:19:00 +0300

    Add files via upload

---
+++ mesto-react/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header className="header">
+        <img className="header__logo" src={logo} alt="Лого" />
+      </header>
+    );
+  }
+  
+  export default Header;
---
+++ mesto-react/src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card.js";
+
+function Main(props) {
+
+
+  <main className="content">
+    <section className="profile">
+      <div className="profile__avatar">
+        <img
+          className="profile__image" src="#" alt="фотография обладателя профиля" />
+      </div>
+      <div className="profile__info">
+        <h1 className="profile__title"></h1>
+        <button type="button" className="profile__edit-button button"></button>
+        <p className="profile__subtitle"></p>
+      </div>
+      <button type="button" className="profile__add-button button"></button>
+    </section>
+
+    <section className="elements">
+    </section>
+  </main>
+}
