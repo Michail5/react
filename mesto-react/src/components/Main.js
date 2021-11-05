47b082b34495d6d4e6712614e8fdb92ceb13b935
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-05 11:48:08 +0300

    Add files via upload

--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -1,71 +1,80 @@
-import React from "react";
-import Api, { apiData } from '../utils/Api.js';
-import Card from "./Card.js";
-
+import React from 'react';
+import { apiData } from '../utils/Api';
+import Card from './Card';
 
 function Main(props) {
+  const [userName, handleGetUserName] = React.useState('');
+  const [userDescription, handleGetUserDescription] = React.useState('');
+  const [userAvatar, handleGetUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState('');
 
-  const [userName, handleGetUserName] = React.useState(null);
-  const [userDescription, handleGetUserDescription] = React.useState(null);
-  const [userAvatar, handleGetUserAvatar] = React.useState(null);
-  const [cards, setCards] = React.useState([]);
+
 
   React.useEffect(() => {
-    
+    // запрос в API за пользовательскими данными
     apiData.getUserData()
-    .then(res => {
-      handleGetUserName(res.name);
-      handleGetUserDescription(res.about); 
-      handleGetUserAvatar(res.avatar);
-    })
-    .catch((err) => {
-      console.log(err); 
-    })
-  });
-
-  React.useEffect(() => {
-    
-    apiData.getInitialCards()
-    .then(items => {
-      setCards(items);
-    })
-    .catch((err) => {
-      console.log(err); 
-      return [];
-    })
+      .then(res => {
+        handleGetUserName(res.name);
+        handleGetUserDescription(res.about);
+        handleGetUserAvatar(res.avatar);
+      })
+      .catch((err) => {
+        console.log(err); // "Что-то пошло не так: ..."
+      })
   }, []);
 
+  React.useEffect(() => {
+    // запрос в API за карточками мест
+    apiData.getInitialCards()
+      .then(items => {
+        setCards(items);
+      })
+      .catch((err) => {
+        console.log(err); // "Что-то пошло не так: ..."
+      })
+  }, []);
 
   function CardList(props) {
     const cards = props.cards;
-    const listCards = cards.map((card, i) =>
-    <Card card={card} key={i} onCardClick={props.onFunctionClick} />
-    );
-    return (
-      <section className="elements">
-        {listCards}
-      </section>
-    );
-  }
+    const listCards = cards.map((card,i) =>
+    <Card card={card} onCardClick={props.onCardClick} key={card._id} />
+  );
   return (
-    
-    <main className="content">
-      <section className="profile">
-        <div className="profile__avatar">
-          <img
-            className="profile__image" src={userAvatar} alt={userName} onClick={props.onEditAvatar}/>
-        </div>
-        <div className="profile__info">
-          <h1 className="profile__title">{userName}</h1>
-          <button type="button" className="profile__edit-button button" onClick={props.onEditProfile}></button>
-          <p className="profile__subtitle">{userDescription}</p>
-        </div>
-        <button type="button" className="profile__add-button button" onClick={props.onAddPlace}></button>
-      </section>
-
-      <CardList cards={cards} onFunctionClick={props.onCardClick}/> 
-</main>
+    <ul className="elements__list">
+      {listCards}
+    </ul>
   );
 }
-
+  return (
+    <main className="content">
+      <section className="profile">
+        <div
+          onClick={props.onEditAvatar}
+          className="profile__avatar-container"
+        >
+          <img
+            src={userAvatar}
+            alt="Жак-Ив Кусто"
+            className="profile__avatar"
+          />
+        </div>
+        <div className="profile__info">
+          <h1 className="profile__name">{userName}</h1>
+          <p className="profile__description">{userDescription}</p>
+          <button
+            onClick={props.onEditProfile}
+            type="button"
+            className="profile__edit-button"
+          />
+        </div>
+        <button
+          onClick={props.onAddPlace}
+          type="button"
+          className="profile__add-button"
+        />
+      </section>
+     
+    </main>
+  )
+}
 export default Main;
